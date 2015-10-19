16d2415d82cc2c138ae44193565813636cec2ce1
ElijahButers <Elijahbuters@gmail.com>
2015-10-19 16:26:57 +0300

    Added:

--- TriviaGameSwiftDemo/ViewController.swift
+++ TriviaGameSwiftDemo/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    
+    @IBOutlet weak var labelQuestion: UILabel!
+    @IBOutlet weak var labelScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
