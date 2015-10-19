4c526af2a66e1abfd30fd9a372740ff35fd3cce4
ElijahButers <Elijahbuters@gmail.com>
2015-10-19 23:03:09 +0300

    Added:
    var currentCorrectAnswerIndex: Int = 0
    
    func loadQuestion(index: Int) {
    
    .......
    	labelQuestion.text = question as String
    
            let indices: [Int] = [0, 1, 2, 3]
            let newSequence = shuffle(indices)
            var i: Int = 0
            for (i = 0; i < newSequence.count; i++) {
    
                let index = newSequence[i]
                if (index == 0) {
    
                    //we need to store the correct answer index
                    currentCorrectAnswerIndex = i
    }
    
    let answer = arr.objectAtIndex(index) as! NSString
                switch(i) {
    
                case 0:
                    buttonA.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
                    break;
                case 1:
                    buttonB.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
                    break;
                case 2:
                    buttonC.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
                    break;
                case 3:
                    buttonD.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
                    break;
                default:
                    break;
                }
            }
    
    func shuffle<C: MutableCollectionType where C.Index == Int>(var list: C) -> C {
    
            let count = list.count  //Swift2, list.count; newElements() - not working
            for i in 0..<(count - 1) {
                let j = Int(arc4random_uniform(UInt32(count - i))) + i //
                guard i != j else { continue } //Swift2.0, guard i != j else { continue }
                swap(&list[i], &list[j])
            }
    
            return list
    
            }

--- TriviaGameSwiftDemo/ViewController.swift
+++ TriviaGameSwiftDemo/ViewController.swift
@@ -21,14 +21,19 @@ class ViewController: UIViewController {
     
     var score: Int = 0
     var allEntries: NSArray!
+    var currentCorrectAnswerIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //let randomNumber = Int(arc4random_uniform(UInt32(allEntries.count)))
+        
         loadScore()
         loadAllQuestionsAndAnswers()
         loadQuestion(1)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -73,11 +78,58 @@ class ViewController: UIViewController {
         
         print(question)
         print(arr)
+        
+        labelQuestion.text = question as String
+        
+        let indices: [Int] = [0, 1, 2, 3]
+        let newSequence = shuffle(indices)
+        var i: Int = 0
+        for (i = 0; i < newSequence.count; i++) {
+            
+            let index = newSequence[i]
+            if (index == 0) {
+                
+                //we need to store the correct answer index
+                currentCorrectAnswerIndex = i
+            }
+            
+            
+            let answer = arr.objectAtIndex(index) as! NSString
+            switch(i) {
+                
+            case 0:
+                buttonA.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
+                break;
+            case 1:
+                buttonB.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
+                break;
+            case 2:
+                buttonC.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
+                break;
+            case 3:
+                buttonD.setTitle(answer as String, forState: UIControlState.Normal) //Swift 2, answer as String
+                break;
+            default:
+                break;
+            }
+        }
+        
     }
-
+    
+    func shuffle<C: MutableCollectionType where C.Index == Int>(var list: C) -> C {
+        
+        let count = list.count  //Swift2, list.count; newElements() - not working
+        for i in 0..<(count - 1) {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i //
+            guard i != j else { continue } //Swift2.0, guard i != j else { continue }
+            swap(&list[i], &list[j])
+        }
+        
+        return list
+            
+        }
+    
 }
-
-
 
 
 
